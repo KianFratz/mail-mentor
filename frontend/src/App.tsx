a25ec592344e8818de5aef2c5f5caa7edc4bfd4f
dom.dom <dyasuham@gmail.com>
2026-06-06 21:02:59 +0800

    - Setup frontend with Reac + Vite + TypeScript template - Add frontend React + Vite scaffold - Install TailwindCSS - Setup absolute path importmap

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen text-center">
+      <p className="text-red-500">Hello world! TailwindCSS test</p>
+    </div>
+  )
+}
+
+export default App
